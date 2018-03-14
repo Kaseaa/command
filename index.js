938d08c15159e575492266ab0ec75465c95b6d18
Kasea <1997xeela@gmail.com>
2018-03-15 00:16:22 +0100

    templeOS next

--- index.js
+++ index.js
@@ -1,241 +1,259 @@
-'use strict'
+const config = require('./config.json');
+const DEFAULT_HOOK_SETTINGS_ON_CHAT = {order: 10};
+const REPLACE_SYMBOLS_ON_MESSAGE = {
+	"&": "&amp;",
+	"<": "&lt;",
+	">": "&gt;"
+};
 
-const PRIVATE_CHANNEL_INDEX = 7,
-	PRIVATE_CHANNEL_ID = -2 >>> 0,
-	PRIVATE_CHANNEL_NAME = 'Proxy',
-	PUBLIC_ENABLE = true,
-	PUBLIC_MATCH = /^!([^!].*)$/,
-	LOGIN_MESSAGE = true
+const PRIVATE_CHANNEL_INDEX = 7;
+const PRIVATE_CHANNEL_ID = -2 >>> 0;
 
 class Command {
 	constructor(dispatch) {
-		this.dispatch = dispatch
+		let currentHookNum = 0xffffffff;
+		let hooks = {};
+		let loaded = false;
 
-		this.loaded = false
-		this.hooks = {}
+		/** Functions for this class **/
+		this.add = (cmd, callback, id) => {
+			if(Array.isArray(cmd)) {
+				for(let i in cmd) this.add(cmd[i], callback, id);
+				return;
+			}
 
-		dispatch.hook('S_LOGIN', 2, () => { this.loaded = false })
+			if(!cmd || !callback) return console.error(`Failed to add command ${cmd}.`);
+			// Case insensitivity \o/
+			cmd = cmd.toLowerCase();
+			if(!hooks[cmd]) hooks[cmd] = [];
 
-		dispatch.hook('S_LOAD_CLIENT_USER_SETTING', 1, () => {
-			if(!this.loaded && (this.loaded = true))
-				process.nextTick(() => {
-					dispatch.toClient('S_JOIN_PRIVATE_CHANNEL', 1, {
-						index: PRIVATE_CHANNEL_INDEX,
-						id: PRIVATE_CHANNEL_ID,
-						unk: [],
-						name: PRIVATE_CHANNEL_NAME
-					})
+			hooks[cmd].push({
+				"id": id || --currentHookNum,
+				"callback": callback
+			});
+		};
 
-					if(LOGIN_MESSAGE) this.message('TERA Proxy enabled. Client version: ' + this.dispatch.base.protocolVersion)
-				})
-		})
+		this.remove = (cmd, id) => {
+			if(Array.isArray(cmd)) {
+				for(let i in cmd) this.remove(cmd[i], id);
+				return;
+			}
 
-		dispatch.hook('S_JOIN_PRIVATE_CHANNEL', 1, event => event.index == PRIVATE_CHANNEL_INDEX ? false : undefined)
-		dispatch.hook('C_LEAVE_PRIVATE_CHANNEL', 1, event => event.index == PRIVATE_CHANNEL_INDEX ? false : undefined)
+			if(!id || !cmd) return console.error(`Invalid id for removal on command ${cmd}`);
 
-		dispatch.hook('C_REQUEST_PRIVATE_CHANNEL_INFO', 1, event => {
-			if(event.channelId == PRIVATE_CHANNEL_ID) {
+			for(let idx in hooks[cmd] || []) {
+				if(hooks[cmd][idx].id === id) return hooks[cmd].slice(idx, 1);
+			}
+		};
+
+		this.message = (msg) => {
+			if(!msg) return;
+
+			for(let i = msg.length - 1; i > 0; i--) {
+				if(REPLACE_SYMBOLS_ON_MESSAGE[msg[i]]) {
+					msg = msg.slice(0, i) + REPLACE_SYMBOLS_ON_MESSAGE[msg[i]] + msg.slice(i + 1);
+				}
+			}
+
+			dispatch.toClient('S_PRIVATE_CHAT', 1, {
+				channel: PRIVATE_CHANNEL_ID,
+				authorID: 0,
+				authorName: config.authorName,
+				message: msg
+			});
+		};
+
+		this.exec = (str, requiresPrefix=true) => {
+			str = parseString(str);
+			let data = getStringCommandInfo(str, requiresPrefix);
+			if(data.length) {
+				// Case insensitivity \o/
+				let cmd = data[0].toLowerCase();
+				data = data.splice(0, 1);
+
+				for(let idx in hooks[cmd]) {
+					try {
+						hooks[cmd][idx].callback(...data);
+					}catch(e) {
+						console.log(e);
+						this.message(`Error on command ${cmd}`);
+						this.message(e);
+					}
+				}
+				return false;
+			}
+			return true;
+		};
+
+
+		/** Helper functions **/
+		function parseString(str) {
+			return stripFont(str);
+		}
+
+		function getStringCommandInfo(str, requiresPrefix) {
+			let arr = parseArgs(str);
+			if(!arr || arr.length === 0) return [];
+
+			// Check for prefix
+			let prefixInfo = { found: false, prefix: "" };
+			for(let prefix of config.commandPrefixes) {
+				if(arr[0].indexOf(prefix) === 0) prefixInfo = {found: true, prefix};
+			}
+
+			if(prefixInfo.found) {
+				arr[0] = arr[0].replace(prefixInfo.prefix, '');
+			}else if(requiresPrefix) return [];
+
+			return arr;
+		}
+
+		/** Packet handling **/
+		/** Handle chat **/
+		this.messagePacket = (e) => {
+			if(e.channel == 11 + PRIVATE_CHANNEL_INDEX) {
+				this.exec(e.message, false);
+				return false;
+			}else if(config.enableCommandsInPublicChats) {
+				return this.exec(e.message, true);
+			}
+		};
+
+		dispatch.hook('C_CHAT', 1, DEFAULT_HOOK_SETTINGS_ON_CHAT, this.messagePacket);
+		dispatch.hook('C_WHISPER', 1, DEFAULT_HOOK_SETTINGS_ON_CHAT, this.messagePacket);
+
+		// Wrapping this in a try statement due to potential missing opcodes
+		try{
+			dispatch.hook('C_OP_COMMAND', 1, e=> this.exec(e.cmd, false));
+			dispatch.hook('C_ADMIN', 1, e=> this.exec(e.command, false));
+		}catch(e) {}
+
+		/** Handle misc **/
+
+		// Set Loaded on login
+		dispatch.hook('S_LOGIN', ()=> { loaded = false; });
+
+		// Join channel
+		this.__joinChannel = () => {
+			dispatch.toClient('S_JOIN_PRIVATE_CHANNEL', 1, {
+				index: PRIVATE_CHANNEL_INDEX,
+				id: PRIVATE_CHANNEL_ID,
+				unk: [],
+				name: config.prefix
+			});
+			if(config.loginMessage) {
+				this.message(`TERA Proxy enabled. Client version: ${dispatch.base.protocolVersion}. Patch version: ${dispatch.base.majorPatchVersion}`);
+			}
+		}
+
+		dispatch.hook('S_LOAD_CLIENT_USER_SETTING', ()=> {
+			if(!loaded) {
+				loaded = true;
+				process.nextTick(this.__joinChannel);
+			}
+		});
+
+		// Silence packets if we're using them for this module
+		this.__silenceChannelPacket = (e) => e.index !== PRIVATE_CHANNEL_INDEX;
+		dispatch.hook('S_JOIN_PRIVATE_CHANNEL', 1, this.__silenceChannelPacket);
+		dispatch.hook('C_LEAVE_PRIVATE_CHANNEL', 1, this.__silenceChannelPacket);
+
+		// Reply to the packet for our channel
+		dispatch.hook('C_REQUEST_PRIVATE_CHANNEL_INFO', 1, e=> {
+			if(e.channelId === PRIVATE_CHANNEL_ID) {
 				dispatch.toClient('S_REQUEST_PRIVATE_CHANNEL_INFO', 1, {
 					owner: 1,
 					password: 0,
 					members: [],
 					friends: []
-				})
-				return false
+				});
+				return false;
 			}
-		})
+		});
 
-		let lastError,
-			hookCommand = message => {
-				let args = null
-
-				try {
-					args = parseArgs(stripOuterHTML(message))
-				}
-				catch(e) {
-					return 'Syntax error: ' + e.message
-				}
-
-				try {
-					if(!this.exec(args)) return 'Unknown command "' + args[0] + '".'
-				}
-				catch(e) {
-					this.message('Error running callback for command "' + args[0] + '".')
-					console.error(e)
-				}
+		// Hook our own command \o/
+		this.add(config.showCommandsCommands, ()=> {
+			for(let key in hooks) {
+				this.message(`Command: ${key}`);
 			}
-
-		dispatch.hook('C_CHAT', 1, {order: -10}, event => {
-			if(event.channel == 11 + PRIVATE_CHANNEL_INDEX) {
-				lastError = hookCommand(event.message)
-				if(!lastError) return false
-			}
-			else if(PUBLIC_ENABLE) {
-				let str = PUBLIC_MATCH.exec(stripOuterHTML(event.message))
-
-				if(str) {
-					lastError = hookCommand(str[1])
-					if(!lastError) return false
-				}
-			}
-		})
-
-		// Let other modules handle possible commands before we silence them
-		dispatch.hook('C_CHAT', 1, {order: 10, filter: {silenced: null}}, event => {
-			if(lastError) {
-				if(!event.$silenced) this.message(lastError)
-				lastError = undefined
-				return false
-			}
-		})
-
-		if(PUBLIC_ENABLE) {
-			dispatch.hook('C_WHISPER', 1, {order: -10}, event => {
-				let str = PUBLIC_MATCH.exec(stripOuterHTML(event.message))
-
-				if(str) {
-					lastError = hookCommand(str[1])
-					if(!lastError) return false
-				}
-			})
-
-			// Let other modules handle possible commands before we silence them
-			dispatch.hook('C_WHISPER', 1, {order: 10, filter: {silenced: null}}, event => {
-				if(lastError) {
-					if(!event.$silenced) this.message(lastError)
-					lastError = undefined
-					return false
-				}
-			})
-		}
-	}
-
-	exec(str) {
-		let args = Array.isArray(str) ? str : parseArgs(str)
-
-		if(args.length === 0) return false
-
-		let cmd = this.hooks[args[0].toLowerCase()]
-
-		if(cmd) {
-			args = args.slice()
-			args.shift()
-			cmd(...args)
-			return true
-		}
-
-		return false
-	}
-
-	add(cmd, cb) {
-		if(Array.isArray(cmd)) {
-			for(let c of cmd) this.add(c, cb)
-			return
-		}
-
-		if(typeof cmd !== 'string') throw new Error('Command must be a string or array of strings')
-		if(cmd === '') throw new Error('Command must not be an empty string')
-
-		if(this.hooks[cmd = cmd.toLowerCase()]) throw new Error('Command already registered:', cmd)
-
-		this.hooks[cmd] = cb
-	}
-
-	remove(cmd) {
-		if(Array.isArray(cmd)) {
-			for(let c of cmd) this.remove(c)
-			return
-		}
-
-		if(typeof cmd !== 'string') throw new Error('Command must be a string or array of strings')
-		if(cmd === '') throw new Error('Command must not be an empty string')
-
-		delete this.hooks[cmd.toLowerCase()]
-	}
-
-	message(msg) {
-		this.dispatch.toClient('S_PRIVATE_CHAT', 1, {
-			channel: PRIVATE_CHANNEL_ID,
-			authorID: 0,
-			authorName: '',
-			message: msg
-		})
+		}, "Kasea is a genius.");
 	}
 }
 
-function stripOuterHTML(str) {
-	return str.replace(/^<[^>]+>|<\/[^>]+><[^\/][^>]*>|<\/[^>]+>$/g, '')
+function stripFont(str) {
+	str = str.replace('</FONT>', '');
+	return str.replace('<FONT>', '');
 }
 
 function parseArgs(str) {
-	let args = [],
-		arg = '',
-		quote = ''
+	let args = [];
+	let arg = '';
+	let quote = '';
 
-	let parseHTML = /.*?<\/.*?>/g
+	let parseHTML = /.*?<\/.*?>/g;
 
 	for(let i = 0, c = ''; i < str.length; i++) {
-		c = str[i]
+		c = str[i];
 
 		switch(c) {
 			case '<':
-				parseHTML.lastIndex = i + 1
+				parseHTML.lastIndex = i + 1;
 
-				let len = parseHTML.exec(str)
+				let len = parseHTML.exec(str);
 
-				if(!len) throw new Error('HTML parsing failure')
+				if(!len) throw new Error('HTML parsing failure');
 
-				len = len[0].length
-				arg += str.substr(i, len + 1)
-				i += len
-				break
+				len = len[0].length;
+				arg += str.substr(i, len + 1);
+				i += len;
+				break;
 			case '\\':
-				c = str[++i]
+				c = str[++i];
 
-				if(c === undefined) throw new Error('Unexpected end of line')
+				if(c === undefined) throw new Error('Unexpected end of line');
 
-				arg += c
-				break
+				arg += c;
+				break;
 			case '\'':
 			case '"':
 				if(arg === '' && quote === '') {
-					quote = c
-					break
+					quote = c;
+					break;
 				}
 				if(quote === c) {
-					quote = ''
-					break
+					quote = '';
+					break;
 				}
-				arg += c
-				break
+				arg += c;
+				break;
 			case ' ':
 				if(quote === '') {
 					if(arg !== '') {
-						args.push(arg)
-						arg = ''
+						args.push(arg);
+						arg = '';
 					}
-					break
+					break;
 				}
 			default:
-				arg += c
+				arg += c;
 		}
 	}
 
 	if(arg !== '') {
-		if(quote !== '') throw new Error('Expected ' + quote)
+		if(quote !== '') throw new Error('Expected ' + quote);
 
-		args.push(arg)
+		args.push(arg);
 	}
 
-	return args
+	return args;
 }
 
-let map = new WeakMap()
+let map = new WeakMap();
 
 module.exports = function Require(dispatch) {
-	if(map.has(dispatch.base)) return map.get(dispatch.base)
+	if(map.has(dispatch.base)) return map.get(dispatch.base);
 
-	let command = new Command(dispatch)
-	map.set(dispatch.base, command)
-	return command
+	let command = new Command(dispatch);
+	map.set(dispatch.base, command);
+	return command;
 }
